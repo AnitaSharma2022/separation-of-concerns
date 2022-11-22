0dda89faf11a09d0843fa795b29ac601b883ce03
Anita <103582907+AnitaSharma2022@users.noreply.github.com>
2022-11-22 12:50:13 +0100

    added exercise 01dom-elements

--- 2-isolate-and-integrate/1-isolate/01-dom-elements/exercises/fill-in-the-blanks.js
+++ 2-isolate-and-integrate/1-isolate/01-dom-elements/exercises/fill-in-the-blanks.js
@@ -8,20 +8,20 @@
 */
 
 const _1_element = document.createElement('input');
-const _1_test = _1_element.nodeName === 'input';
+const _1_test = _1_element.nodeName === 'INPUT';
 console.assert(_1_test, 'Test 1');
 
-const _2_element = document.createElement('input');
-const _2_test = _2_element.nodeName === 'input';
+const _2_element = document.createElement('inPUT');
+const _2_test = _2_element.nodeName === 'INPUT';
 console.assert(_2_test, 'Test 2');
 
-const _3_element = document.createElement('ARTICLE');
+const _3_element = document.createElement('article');
 const _3_test = _3_element.nodeName === 'ARTICLE';
 console.assert(_3_test, 'Test 3');
 
-const _4_element = document.createElement('p');
-package.innerHTML='party palace';
-const _4_test = _4_element.innerHTML === 'party palace';
+const a_4_element = document.createElement('p');
+a_4_element.innerHTML='party palace';
+const _4_test = a_4_element.innerHTML === 'party palace';
 console.assert(_4_test, 'Test 4');
 
 const a_5_element = document.createElement('p');
